e32453241af0551c22aa968a45342e97b36edef5
Edwin Otten <otten.nl@gmail.com>
2015-03-17 20:30:12 +0100

    version 1.0

---
+++ index.php
@@ -0,0 +1,139 @@
+<html>
+<head>
+
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.95.3/css/materialize.min.css" media="screen,projection" rel="stylesheet" type="text/css"><!--Let browser know website is optimized for mobile-->
+  <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
+  <meta content="IE=EmulateIE9" http-equiv="X-UA-Compatible">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="theme-color" content="#516873">
+
+  <link rel="apple-touch-icon-precomposed" sizes="57x57" href="http://categorie.avgroenester.nl/favicon/apple-touch-icon-57x57.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://categorie.avgroenester.nl/favicon/apple-touch-icon-114x114.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://categorie.avgroenester.nl/favicon/apple-touch-icon-72x72.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://categorie.avgroenester.nl/favicon/apple-touch-icon-144x144.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="60x60" href="http://categorie.avgroenester.nl/favicon/apple-touch-icon-60x60.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="120x120" href="http://categorie.avgroenester.nl/favicon/apple-touch-icon-120x120.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="76x76" href="http://categorie.avgroenester.nl/favicon/apple-touch-icon-76x76.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="152x152" href="http://categorie.avgroenester.nl/favicon/apple-touch-icon-152x152.png" />
+  <link rel="icon" type="image/png" href="http://categorie.avgroenester.nl/favicon/favicon-196x196.png" sizes="196x196" />
+  <link rel="icon" type="image/png" href="http://categorie.avgroenester.nl/favicon/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/png" href="http://categorie.avgroenester.nl/favicon/favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="http://categorie.avgroenester.nl/favicon/favicon-16x16.png" sizes="16x16" />
+  <link rel="icon" type="image/png" href="http://categorie.avgroenester.nl/favicon/favicon-128.png" sizes="128x128" />
+  <meta name="application-name" content="Atletiek categorie berekenen"/>
+  <meta name="msapplication-TileColor" content="#C2473B" />
+  <meta name="msapplication-TileImage" content="http://categorie.avgroenester.nl/favicon/mstile-144x144.png" />
+  <meta name="msapplication-square70x70logo" content="http://categorie.avgroenester.nl/favicon/mstile-70x70.png" />
+  <meta name="msapplication-square150x150logo" content="http://categorie.avgroenester.nl/favicon/mstile-150x150.png" />
+  <meta name="msapplication-wide310x150logo" content="http://categorie.avgroenester.nl/favicon/mstile-310x150.png" />
+  <meta name="msapplication-square310x310logo" content="http://categorie.avgroenester.nl/favicon/mstile-310x310.png" />
+
+  <title>Atletiek categorie berekenen</title>
+  <link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body class="blue-grey">
+
+  <main>
+    <div class="row">
+      <div class="col offset-l3 l6 offset-m1 m10 s12">
+        <div class="card" id="birthdate-box">
+          <div class="card-content">
+            <span class="card-title teal-text">Atletiek categorie</span>
+            <p>Met deze calculator kun je eenvoudig je atletiek categorie berekenen. Vul je geboortedatum maar eens in!</p>
+          </div>
+
+
+          <div class="card-action">
+            <form id="birthdate-form" name="birthdate-form">
+              <div class="row">
+
+                <div class="input-field col m6 s12">
+                  <select class="browser-default" id="month" name="month">
+                    <option disabled selected>maand</option>
+                    <option value="1">januari</option>
+                    <option value="2">februari</option>
+                    <option value="3">maart</option>
+                    <option value="4">april</option>
+                    <option value="5">mei</option>
+                    <option value="6">juni</option>
+                    <option value="7">juli</option>
+                    <option value="8">augustus</option>
+                    <option value="9">september</option>
+                    <option value="10">oktober</option>
+                    <option value="11">november</option>
+                    <option value="12">december</option>
+                  </select>
+                </div>
+
+                <div class="input-field col m6 s12">
+                  <select class="browser-default" id="year" name="year">
+                    <option disabled selected>jaar</option>
+                    <?php 
+                      $year = date('Y')-4;
+                      for($i=$year; $i > $year-100; $i--) {
+                        echo "<option>$i</option>\n";
+                      }
+                    ?>
+                  </select>
+                </div>
+              </div>
+
+
+              <div class="row">
+                <div class="input-field col s6">
+                  <button class="btn waves-effect waves-light" name="action" type="submit">Bereken <i class="mdi-content-send right"></i></button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
+
+        <div class="section" id="result-section">
+        </div>
+      </div>
+    </div>
+  </main>
+
+
+  <footer class="page-footer blue-grey darken-1">
+    <div class="footer-copyright">
+      <div class="container">
+        &copy; 2015 Copyright Edwin Otten <a class="grey-text text-lighten-4 right" href="https://nl.linkedin.com/pub/edwin-otten/3a/707/328">Edwin on LinkedIn</a>
+      </div>
+    </div>
+  </footer>
+
+  <!--Import jQuery before materialize.js-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script> 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.95.3/js/materialize.min.js" type="text/javascript"></script> 
+  <script src="scripts.js" type="text/javascript"></script>
+  <script>
+    var currentYear = <?php echo date('Y'); ?>;
+    var currentMonth = <?php echo date('m'); ?>;
+    var i = 0;
+    var marginTopBeforeAnimation = 0;
+
+    $(function() {
+      if (isIE()) {
+        document.documentElement.setAttribute('data-useragent', 'IE');
+      }
+
+      $("#birthdate-form").submit(function (event) {
+        event.preventDefault();
+
+        var year = parseInt($("#birthdate-form #year").val());
+        var month = parseInt($("#birthdate-form #month").val());
+
+        if (isInteger(year) && isInteger(month)) {
+            displayCategories(year, month);
+        }
+      });
+
+    });
+  </script>
+
+</body>
+</html>
---
+++ scripts.js
@@ -0,0 +1,138 @@
+function isInteger(num) {
+return (num ^ 0) === num;
+}
+
+function isIE() { return ((navigator.appName == 'Microsoft Internet Explorer') || ((navigator.appName == 'Netscape') && (new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})").exec(navigator.userAgent) != null))); }
+
+function stringContains(string, substring) {
+return (string.indexOf(substring) > -1);
+}
+
+function displayCategories(birthYear, birthMonth) {
+    $("#birthdate-form button[type=submit]").prop('disabled', true);
+
+    if (i >= 1) {
+        var margin = parseInt($('#category-' + i).css('margin'));
+        marginTopBeforeAnimation = margin;
+        var padding = parseInt($('#category-' + i).css('padding'));
+        var height = $('#category-' + i).height() + 2 * margin + 2 * padding;
+
+        $('#category-' + i).animate({
+            'marginTop': height
+        }, 500, function () {
+            fadeInCategories(birthYear, birthMonth);
+        });
+    } else {
+        fadeInCategories(birthYear, birthMonth)
+    }
+
+}
+
+function fadeInCategories(birthYear, birthMonth) {
+    var thisSeason = currentYear;
+    var nextSeason = currentYear + 1;
+
+    if (currentMonth >= 10) {
+        thisSeason = currentYear + 1;
+        nextSeason = currentYear + 2;
+    }
+
+    i++;
+    var table = '<div class="card" id="category-' + i + '" style="display: none;">'+
+    '          <div class="card-content">'+
+    '            <span class="card-title teal-text">Categorie&euml;n <small>voor geboortedatum ' + birthMonth + '-' + birthYear + '<\/small><\/span>'+
+    '            <table>'+
+    '              <thead>'+
+    '                <tr>'+
+    '                  <th data-field="season">Seizoen<\/th>'+
+    '                  <th data-field="year">Seizoenjaar<\/th>'+
+    '                  <th data-field="category">Categorie<\/th>'+
+    '                <\/tr>'+
+    '              <\/thead>'+
+    '              <tbody>'+
+    '                <tr>'+
+    '                  <td>huidig<\/td>'+
+    '                  <td>' + thisSeason + '<\/td>'+
+    '                  <td>' + calculateCategory(birthYear, birthMonth, thisSeason) + '<\/td>'+
+    '                <\/tr>'+
+    '                <tr>'+
+    '                  <td>volgend<\/td>'+
+    '                  <td>' + nextSeason + '<\/td>'+
+    '                  <td>' + calculateCategory(birthYear, birthMonth, nextSeason) + '<\/td>'+
+    '                <\/tr>'+
+    '              <\/tbody>'+
+    '            <\/table>'+
+    '          <\/div>'+
+    '        <\/div>';
+
+    $('#category-' + (i - 1)).css('margin-top', marginTopBeforeAnimation+'px');
+    $('#result-section').prepend(table);
+
+    $('#category-' + i).fadeIn(300, function () {
+        $("#birthdate-form button[type=submit]").prop('disabled', false);
+    });
+}
+
+function calculateCategory(birthYear, birthMonth, seasonYear) {
+    var ageYears = seasonYear - birthYear;
+    var category = "niet gevonden";
+
+    if (ageYears >= 35) {
+        category = "masters";
+    } else if (ageYears < 35 && ageYears >= 20) {
+        category = "Senioren";
+    } else if (ageYears < 20) {
+        switch (ageYears) {
+            case 4:
+                category = "Klatjes";
+                break;
+            case 5:
+                category = "Klatjes";
+                break;
+            case 6:
+                category = "Mini pupillen";
+                break;
+            case 7:
+                category = "Mini pupillen";
+                break;
+            case 8:
+                category = "C-pupillen";
+                break;
+            case 9:
+                category = "B-pupillen";
+                break;
+            case 10:
+                category = "A-pupillen";
+                break;
+            case 11:
+                category = "A-pupillen";
+                break;
+            case 12:
+                category = "D-junioren";
+                break;
+            case 13:
+                category = "D-junioren";
+                break;
+            case 14:
+                category = "C-junioren";
+                break;
+            case 15:
+                category = "C-junioren";
+                break;
+            case 16:
+                category = "B-junioren";
+                break;
+            case 17:
+                category = "B-junioren";
+                break;
+            case 18:
+                category = "A-junioren";
+                break;
+            case 19:
+                category = "A-junioren";
+                break;
+        }
+    }
+
+    return category;
+}
